b6e64bd0b6f9601820727792054f8edf426d9330
mmayet <mayetm93@gmail.com>
2014-08-13 20:45:32 -0700

    Revert "map changes"
    This reverts commit d3606f9a89c321f00416d63fdffa898ff1b5cd05.

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,16 @@
 var top_menu_height = 0;
+jQuery(function($) {
+        $(document).ready( function() {
 
-$(document).ready(function() {
-  run();
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+            'callback=initialize';
+        document.body.appendChild(script);
+    });
 });
 
-function run() {
-  jQuery(function($) {
-    ///$(document).ready( function() {
-
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-      'callback=initialize';
-      document.body.appendChild(script);
-    //});
-  });
-
-  function initialize() {
+function initialize() {
     var myLatlng = new google.maps.LatLng(33.867218,-117.933460);
     var mapOptions = {
       zoom: 17,
@@ -30,8 +24,8 @@ function run() {
       map: map,
       title: 'VMC Masjid'
     });
-  }
 }
+
 /*
 $(".jumbotron").mouseenter(function() {
   $( this ).css("background-color", "rgb(166, 206, 244)");
