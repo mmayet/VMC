d3606f9a89c321f00416d63fdffa898ff1b5cd05
mmayet <mayetm93@gmail.com>
2014-08-13 20:43:06 -0700

    map changes

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,22 @@
 var top_menu_height = 0;
-jQuery(function($) {
-        $(document).ready( function() {
 
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-            'callback=initialize';
-        document.body.appendChild(script);
-    });
+$(document).ready(function() {
+  run();
 });
 
-function initialize() {
+function run() {
+  jQuery(function($) {
+    ///$(document).ready( function() {
+
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+      'callback=initialize';
+      document.body.appendChild(script);
+    //});
+  });
+
+  function initialize() {
     var myLatlng = new google.maps.LatLng(33.867218,-117.933460);
     var mapOptions = {
       zoom: 17,
@@ -24,8 +30,8 @@ function initialize() {
       map: map,
       title: 'VMC Masjid'
     });
+  }
 }
-
 /*
 $(".jumbotron").mouseenter(function() {
   $( this ).css("background-color", "rgb(166, 206, 244)");
